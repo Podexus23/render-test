a481ef5715fffa7789691e3b35c4a6f5acd1d5ba
Podexus23 <santon23@gmail.com>
2025-02-19 19:08:12 +0300

    fix: workable fix cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.writeHead(200, { "Content-type": "application/json" });
   res.end(JSON.stringify({ data: "hello" }));
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log("server is running on 3000");
 });
